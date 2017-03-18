a6a430fe21fa138def3d65497e545783ca823b72
Rodrigo Orem <orem.rodrigo@gmail.com>
2017-03-18 01:25:04 -0300

    Indentação de 2 ao invés de 4 espaços

--- src/background.js
+++ src/background.js
@@ -1,128 +1,128 @@
 // Script blocking
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {cancel: true};
-    },
-    {
-        urls: [
-            // O Estado de S. Paulo
-            "*://*.estadao.com.br/paywall/*",
+  function(details) {
+    return {cancel: true};
+  },
+  {
+    urls: [
+      // O Estado de S. Paulo
+      "*://*.estadao.com.br/paywall/*",
 
-            // Folha de S.Paulo
-            "*://paywall.folha.uol.com.br/*",
-            "*://static.folha.uol.com.br/paywall/*",
+      // Folha de S.Paulo
+      "*://paywall.folha.uol.com.br/*",
+      "*://static.folha.uol.com.br/paywall/*",
 
-            // O Globo
-            "*://ogjs.infoglobo.com.br/*/js/controla-acesso-aux.js",
-            "*://cdn.tinypass.com/api/tinypass.min.js",
+      // O Globo
+      "*://ogjs.infoglobo.com.br/*/js/controla-acesso-aux.js",
+      "*://cdn.tinypass.com/api/tinypass.min.js",
 
-            // Gazeta do Povo
-            "*://*.gazetadopovo.com.br/conta/public/js/connect_api.js*",
-            "*://*.gazetadopovo.com.br/conta/going/api/paywall/*",
+      // Gazeta do Povo
+      "*://*.gazetadopovo.com.br/conta/public/js/connect_api.js*",
+      "*://*.gazetadopovo.com.br/conta/going/api/paywall/*",
 
-            // Zero Hora
-            "*://zh.clicrbs.com.br/it/js/paid-content-config.js*",
-            "*://www.rbsonline.com.br/cdn/scripts/paywall.min.js*",
+      // Zero Hora
+      "*://zh.clicrbs.com.br/it/js/paid-content-config.js*",
+      "*://www.rbsonline.com.br/cdn/scripts/paywall.min.js*",
 
-            // Correio Popular
-            "*://correio.rac.com.br/includes/js/novo_cp/fivewall.js*",
+      // Correio Popular
+      "*://correio.rac.com.br/includes/js/novo_cp/fivewall.js*",
 
-            // O Estado do Maranhão
-            "*://dashboard.tinypass.com/xbuilder/experience/load*",
-            "http://assets.imirante.com/2.0/oestadoma/js/jquery.login.min.js",
+      // O Estado do Maranhão
+      "*://dashboard.tinypass.com/xbuilder/experience/load*",
+      "http://assets.imirante.com/2.0/oestadoma/js/jquery.login.min.js",
 
-            // Jornal de Novo Hamburgo
-            "*://www.jornalnh.com.br/includes/js/paywall.js*",
-            "*://blockv2.fivewall.com.br/*"
-        ],
-        types: ["script"]
-    },
-    ["blocking"]
+      // Jornal de Novo Hamburgo
+      "*://www.jornalnh.com.br/includes/js/paywall.js*",
+      "*://blockv2.fivewall.com.br/*"
+    ],
+    types: ["script"]
+  },
+  ["blocking"]
 );
 
 // XHR blocking
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {cancel: true};
-    },
-    {
-        urls: [
-            // Diário Catarinense
-            "http://dc.clicrbs.com.br/jornal-2015/jsp/paywall.jspx*",
+  function(details) {
+    return {cancel: true};
+  },
+  {
+    urls: [
+      // Diário Catarinense
+      "http://dc.clicrbs.com.br/jornal-2015/jsp/paywall.jspx*",
 
-            // Jornal de Santa Catarina
-            "http://jornaldesantacatarina.clicrbs.com.br/jornal/jsp/paywall*",
+      // Jornal de Santa Catarina
+      "http://jornaldesantacatarina.clicrbs.com.br/jornal/jsp/paywall*",
 
-            // O Globo
-            "*://cdn.tinypass.com/api/tinypass.min.js"
-        ],
-        types: ["xmlhttprequest"]
-    },
-    ["blocking"]
+      // O Globo
+      "*://cdn.tinypass.com/api/tinypass.min.js"
+    ],
+    types: ["xmlhttprequest"]
+  },
+  ["blocking"]
 );
 
 // Cookie injection
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        chrome.cookies.remove({
-            'url': 'http://jota.info/*',
-            'name': 'articles'
-        });
-    },
-    {
-        urls: [
-            // JOTA
-            "http://jota.info/*"
-        ],
-        types: ["main_frame"]
-    }
+  function(details) {
+    chrome.cookies.remove({
+      'url': 'http://jota.info/*',
+      'name': 'articles'
+    });
+  },
+  {
+    urls: [
+      // JOTA
+      "http://jota.info/*"
+    ],
+    types: ["main_frame"]
+  }
 );
 
 // Referer injection
 function insertHeader(name, value, requestHeaders) {
-    /**
-     * @param {string} name - Name of the header to be inserted
-     * @param {string} value - Value of the header to be inserted
-     * @param {Object[]} requestHeaders - Provided by webRequest
-     *     listeners in callback arg `details.requestHeader`
-     * @param {string} requestHeaders[].name
-     * @param {string} requestHeaders[].value
-     */
-    var headerIndex = requestHeaders.findIndex(x => x.name == name);
+  /**
+   * @param {string} name - Name of the header to be inserted
+   * @param {string} value - Value of the header to be inserted
+   * @param {Object[]} requestHeaders - Provided by webRequest
+   *   listeners in callback arg `details.requestHeader`
+   * @param {string} requestHeaders[].name
+   * @param {string} requestHeaders[].value
+   */
+  var headerIndex = requestHeaders.findIndex(x => x.name == name);
 
-    var newHeader = {name: name, value: value};
-    if (headerIndex == -1)
-        requestHeaders.push(newHeader);
-    else
-        requestHeaders[headerIndex] = newHeader;
+  var newHeader = {name: name, value: value};
+  if (headerIndex == -1)
+    requestHeaders.push(newHeader);
+  else
+    requestHeaders[headerIndex] = newHeader;
 }
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-        var headers = ['Referer'];
+  function(details) {
+    var headers = ['Referer'];
 
-        //if (/https?:\/\/access\.nexojornal\.com/.test(details.url))
-            //headers.push('X-Article-Referrer')
+    //if (/https?:\/\/access\.nexojornal\.com/.test(details.url))
+      //headers.push('X-Article-Referrer')
 
-        headers.forEach(header =>
-            insertHeader(
-                header,
-                'https://www.google.com.br/',
-                details.requestHeaders
-            )
-        );
+    headers.forEach(header =>
+      insertHeader(
+        header,
+        'https://www.google.com.br/',
+        details.requestHeaders
+      )
+    );
 
-        return {requestHeaders: details.requestHeaders};
-    },
-    {
-        urls: [
-            // Nexo
-            //"*://access.nexojornal.com.br/access/content/*",
+    return {requestHeaders: details.requestHeaders};
+  },
+  {
+    urls: [
+      // Nexo
+      //"*://access.nexojornal.com.br/access/content/*",
 
-            // Financial Times
-            "*://www.ft.com/*"
-        ],
-        types: ["xmlhttprequest", "main_frame"]
-    },
-    ["blocking", "requestHeaders"]
+      // Financial Times
+      "*://www.ft.com/*"
+    ],
+    types: ["xmlhttprequest", "main_frame"]
+  },
+  ["blocking", "requestHeaders"]
 );
--- src/content.js
+++ src/content.js
@@ -1,23 +1,23 @@
 // run_at: document_idle
 var code = null;
 if (/([^\/].)?oglobo\.globo\.com/.test(document.location.host))
-    code = 'paywallAtivo = false;';
+  code = 'paywallAtivo = false;';
 
 else if (/www\.economist\.com/.test(document.location.host))
-    code = 'document.cookie = "ec_limit=allow";';
+  code = 'document.cookie = "ec_limit=allow";';
 
 else if (/foreignpolicy\.com/.test(document.location.host)) {
-    code = '\
-      document.getElementById("paywall_bg").remove();\
-      document.body.classList.remove("overlay-no-scroll");\
-      document.body.style.overflow = "visible";\
-      document.documentElement.classList.remove("overlay-no-scroll");\
-    ';
+  code = '\
+    document.getElementById("paywall_bg").remove();\
+    document.body.classList.remove("overlay-no-scroll");\
+    document.body.style.overflow = "visible";\
+    document.documentElement.classList.remove("overlay-no-scroll");\
+  ';
 }
 
 if (code !== null) {
-    var script = document.createElement('script');
-    script.textContent = code;
-    (document.head||document.documentElement).appendChild(script);
-    script.parentNode.removeChild(script);
+  var script = document.createElement('script');
+  script.textContent = code;
+  (document.head||document.documentElement).appendChild(script);
+  script.parentNode.removeChild(script);
 }
