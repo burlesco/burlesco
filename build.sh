54807b62674be955b4b99cc59734113f80112bd4
Rodrigo Orem <orem.rodrigo@gmail.com>
2017-05-15 19:28:07 -0300

    Conserta build script

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 DIST="dist"
-BROWSERS=("chrome" "firefox" "opera")
+BROWSERS=("firefox" "opera")
 
 
 for i in "${BROWSERS[@]}"
 do
   mkdir -p "$DIST/$i/src"
 
-  rm -rf "$DIST/$i/src/*"
+  rm -rf "$DIST/$i/src/"*
   rm "$DIST/$i/extension.zip"
 
   cp -r src/* "$DIST/$i/src"
   mv "$DIST/$i/manifest-$i.json" "$DIST/$i/manifest.json"
-  rm -f "$DIST/$i/manifest-*"
-  zip -j "$DIST/$i/extension.zip" "$DIST/$i/src/*"
+  rm -f "$DIST/$i/"manifest-*
+  zip -j "$DIST/$i/extension.zip" "$DIST/$i/src/"*
 done
