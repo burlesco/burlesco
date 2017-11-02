8239ebf7fd22fa90fc973813ee52f2842b23bd8d
Augusto Resende <augusto.resende@outlook.com>
2017-11-02 16:31:53 -0200

    Limpar todos os cookies

--- src/content.js
+++ src/content.js
@@ -54,15 +54,24 @@ else if (/gauchazh.clicrbs.com.br/.test(document.location.host)) {
             }\
             var myElem = document.getElementsByClassName("wrapper-paid-content")[0];\
             if (myElem !== undefined) {\
-                document.cookie = "";\
+                deleteAllCookies();\
                 localStorage.clear();\
                 carreganoticia();\
             }\
         }, 500);\
     }\
+    function deleteAllCookies() {\
+        var cookies = document.cookie.split(";");\
+        for (var i = 0; i < cookies.length; i++) {\
+            var cookie = cookies[i];\
+            var eqPos = cookie.indexOf("=");\
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;\
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";\
+        }\
+    }\
     checkBloqueio();\
     function onClick() {\
-        document.cookie = "";\
+        deleteAllCookies();\
         localStorage.clear();\
         tm = true;\
         checkBloqueio();\
