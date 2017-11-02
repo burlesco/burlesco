1deb885252cb2307aafacbead6310a16bbfc7b83
Rodrigo Orem <orem.rodrigo@gmail.com>
2017-11-02 19:31:24 -0200

    Volta remoção de cookies e UOL

--- src/content.js
+++ src/content.js
@@ -57,9 +57,20 @@ else if (/gauchazh.clicrbs.com.br/.test(document.location.host)) {
       }, 1000);
     }
 
+    function deleteAllCookies() {
+      var cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      }
+    }
+
     function clearPersistentData() {
       localStorage.clear();
       sessionStorage.clear();
+      deleteAllCookies();
     }
 
     checkPaywall();
