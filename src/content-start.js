41574b9bf76b4cad87f691c5be589bf3d4ad8f84
Augusto Resende <augusto.resende@outlook.com>
2017-11-07 10:38:49 -0300

    Atualiza GauchaZH

---
+++ src/content-start.js
@@ -0,0 +1,37 @@
+// run_at: document_start
+var code = null;
+
+if (/gauchazh.clicrbs.com.br/.test(document.location.host)) {
+  code = `
+    function patchJs(jsurl) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var injectme = this.responseText;
+                injectme = injectme.replace('showSignwall:e.showLoginPaywall','showSignwall:false');
+                injectme = injectme.replace('showPaywall:e.showPaywall','showPaywall:false');
+                injectme = injectme.replace('requestCPF:e.requestCPF || !1','requestCPF:false');
+                injectme = injectme.replace('!e.showLoginPaywall&&!e.showPaywall||!1','true');
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                var textNode = document.createTextNode(injectme);
+                script.appendChild(textNode);
+                document.head.appendChild(script);
+            }
+        };
+        xhttp.open("GET", jsurl, true);
+        xhttp.send();
+    }
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        patchJs(document.getElementsByTagName('script')[1].src);
+    });
+  `;
+}
+
+if (code !== null) {
+  var script = document.createElement('script');
+  script.textContent = code;
+  (document.head||document.documentElement).appendChild(script);
+  script.parentNode.removeChild(script);
+}
