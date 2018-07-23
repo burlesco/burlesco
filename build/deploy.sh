3755029fdbc35582d13e5ff99164158b9813ddae
Rodrigo O <rodorgas@users.noreply.github.com>
2018-07-23 16:55:38 -0300

    Atualiza url do userscript (deploy)
    Mudança necessária após migração do Burlesco do meu perfil para a
    organização burlesco.

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,4 +2,4 @@ curl -X PATCH "https://api.cloudflare.com/client/v4/zones/$cloudflare_zone/pager
      -H "X-Auth-Email: $cloudflare_email" \
      -H "X-Auth-Key: $cloudflare_token" \
      -H "Content-Type: application/json" \
-     --data '{"targets":[{"target":"url","constraint":{"operator":"matches","value":"burles.co\/userscript\/burlesco.user.js*"}}],"actions":[{"id":"forwarding_url","value":{"url":"https:\/\/github.com\/rodorgas\/burlesco\/releases\/download\/'"$TRAVIS_TAG"'\/burlesco.user.js","status_code":302}}],"priority":1,"status":"active"}'
+     --data '{"targets":[{"target":"url","constraint":{"operator":"matches","value":"burles.co\/userscript\/burlesco.user.js*"}}],"actions":[{"id":"forwarding_url","value":{"url":"https:\/\/github.com\/burlesco\/burlesco\/releases\/download\/'"$TRAVIS_TAG"'\/burlesco.user.js","status_code":302}}],"priority":1,"status":"active"}'
