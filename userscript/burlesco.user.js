dceb85b05d24088b81042889eeb06ce5a770d4f1
Rodrigo O <rodorgas@users.noreply.github.com>
2018-10-10 09:45:09 -0300

    Aumenta versão

--- userscript/burlesco.user.js
+++ userscript/burlesco.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Burlesco
 // @namespace    https://burles.co/
-// @version      8.0
+// @version      9.0
 // @description  Leia notícias sem ser assinante, burle o paywall
 // @author       rodorgas & AugustoResende
 // @supportURL   https://burles.co
