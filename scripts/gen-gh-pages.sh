f708543617bca9d20f2e51183dc200f818959297
Rodrigo O <rodorgas@users.noreply.github.com>
2022-10-18 02:03:17 -0300

    test

--- scripts/gen-gh-pages.sh
+++ scripts/gen-gh-pages.sh
@@ -5,7 +5,7 @@ cat > $BUILD_DIR/chromium.xml <<EOL
 <?xml version='1.0' encoding='UTF-8'?>
 <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
   <app appid='magjmaimhngoidhmklnpihkdjkggbpnp'>
-    <updatecheck codebase='https://github.com/burlesco/burlesco/releases/download/${{ github.ref }}/burlesco-chromium.crx' version='${{ github.ref }}' />
+    <updatecheck codebase='https://github.com/burlesco/burlesco/releases/download/$TAG/burlesco-chromium.crx' version='$TAG' />
   </app>
 </gupdate>
 EOL
@@ -17,8 +17,8 @@ cat > $BUILD_DIR/firefox.json <<EOL
     "burlesco@burles.co": {
       "updates": [
         {
-          "version": "${{ github.ref }}",
-          "update_link": "https://github.com/burlesco/burlesco/releases/download/${{ github.ref }}/burlesco-firefox.xpi"
+          "version": "$TAG",
+          "update_link": "https://github.com/burlesco/burlesco/releases/download/$TAG/burlesco-firefox.xpi"
         }
       ]
     }
